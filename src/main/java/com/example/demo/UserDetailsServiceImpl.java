ced633f935586d25d994b636759d4f47f1a5a0ac
littleworld <peter.van.rijn@little-world.nl>
2024-06-04 15:22:28 +0200

    details changed

--- src/main/java/com/example/demo/UserDetailsServiceImpl.java
+++ src/main/java/com/example/demo/UserDetailsServiceImpl.java
@@ -1,21 +1,30 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
   @Autowired
   UserRepository userRepository;
+
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    return new UserDetailsImpl(
-        userRepository.findByUsername(username)
-            .orElseThrow(() -> new UsernameNotFoundException("not found " + username))
-    );
+
+    var user = userRepository.findByUsername(username)
+        .orElseThrow(() -> new UsernameNotFoundException("not found " + username));
+
+    var authorities = Arrays.stream(user.roles.split(","))
+        .map(SimpleGrantedAuthority::new)
+        .toList();
+
+    return new org.springframework.security.core.userdetails.User(user.username, user.password, authorities);
   }
 }
